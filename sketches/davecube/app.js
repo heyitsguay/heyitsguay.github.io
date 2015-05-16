b9b375abb4f1b22f4b3eae0a90e156d676ce346b
Matt Guay <matt.d.guay@gmail.com>
2015-05-15 22:39:59 -0400

    davecube - Another attempt at fixing the process of stopping the cube by single-touching without moving.

--- sketches/davecube/app.js
+++ sketches/davecube/app.js
@@ -478,7 +478,7 @@ function drawScene()
 
 function animate()
 {
-    if(isTouchscreen && fingerDown && !twoFingersDown && ticksSinceTouchMove > 30)
+    if(isTouchscreen && fingerDown && !twoFingersDown && ticksSinceTouchMove > 15)
     {
         xSpeed = 0;
         ySpeed = 0;
