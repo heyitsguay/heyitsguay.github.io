55232baf0209d8c47df641b67dd434ed78dcaa4a
Matthew Guay <matt.d.guay@gmail.com>
2017-12-26 20:54:10 -0500

    Better coincircle mobile layout.

--- coincircle/js/main.js
+++ coincircle/js/main.js
@@ -1,11 +1,16 @@
 $(document).ready(onReady);
 $('#thebutton').click(onClick);
+$('#ain').keyup(onClick);
+$('#nin').keyup(onClick);
 
 var canvas_der, ctx_der;
 var canvas_app, ctx_app;
+var canvas_loc1, ctx_loc1;
+var canvas_loc2, ctx_loc2;
 
 var coinFill = 'rgba(212, 225, 224, 1)';
 var coinStroke = 'rgba(32, 32, 32, 1)';
+var colBackground = 'rgba(192, 192, 197, 1)';
 
 var cWidth, cHeight;
 
@@ -18,7 +23,7 @@ function isMobileDevice() {
 function onClick() {
     var a = parseFloat($('#ain').val());
     var n = parseInt($('#nin').val());
-    if (n > 2) {
+    if (n > 2 && n < 1000 && a > 0 && a < cWidth) {
         var b = drawRing(ctx_app, a, n);
         $('#solution').text(b.toFixed(6));
     }
@@ -32,11 +37,117 @@ function onReady() {
     ctx_der = canvas_der.getContext('2d');
     canvas_app = document.getElementById('app-canvas');
     ctx_app = canvas_app.getContext('2d');
+
+    canvas_loc1 = document.getElementById('loc1-canvas');
+    ctx_loc1 = canvas_loc1.getContext('2d');
+
+    canvas_loc2 = document.getElementById('loc2-canvas');
+    ctx_loc2 = canvas_loc2.getContext('2d');
+
     cWidth = canvas_der.width;
     cHeight = canvas_der.height;
 
     drawThreeCoins(ctx_der);
+    drawLOC1(ctx_loc1);
+    drawLOC2(ctx_loc2);
+
     onClick();
+}
+
+function drawLOC2(ctx) {
+    ctx.fillStyle = colBackground;
+    ctx.fillRect(0, 0, cWidth, cHeight);
+    var tWidth = 250;
+    var tHeight = 275;
+    var tVertexAngle = 2 * Math.atan(0.5 * tWidth / tHeight);
+    var tAngleStart = Math.PI / 2 - tVertexAngle / 2;
+    var tAngleEnd = Math.PI / 2 + tVertexAngle / 2;
+    var xc = cWidth / 2; var yc = cHeight / 2;
+    var x0 = xc - tWidth / 2; var y0 = yc + tHeight / 2;
+    var x1 = xc; var y1 = yc - tHeight / 2;
+    var x2 = xc + tWidth / 2; var y2 = yc + tHeight / 2;
+
+    ctx.beginPath();
+    ctx.strokeStyle = coinStroke;
+    ctx.fillStyle = coinFill;
+    ctx.lineWidth = 2;
+    ctx.moveTo(x0, y0);
+    ctx.lineTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x0, y0);
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(x1, y1, 20, tAngleStart, tAngleEnd);
+    ctx.stroke();
+
+    ctx.fillStyle = 'black';
+
+    var xTheta = x1 - 2;
+    var yTheta = y1 + 30;
+    ctx.fillText('θ', xTheta, yTheta);
+
+    var xZ = x1 - 4;
+    var yZ = y1 + tHeight + 10;
+    ctx.fillText('2b', xZ, yZ);
+
+    var xX = x1 - 22 - 0.25 * tWidth;
+    var yX = y1 + 0.5 * tHeight;
+    ctx.fillText('a+b', xX, yX);
+
+    var xY = x1 + 6 + 0.25 * tWidth;
+    var yY = y1 + 0.5 * tHeight;
+    ctx.fillText('a+b', xY, yY);
+
+}
+
+function drawLOC1(ctx) {
+    ctx.fillStyle = colBackground;
+    ctx.fillRect(0, 0, cWidth, cHeight);
+    var tWidth = 250;
+    var tHeight = 275;
+    var tVertexAngle = 2 * Math.atan(0.5 * tWidth / tHeight);
+    var tAngleStart = Math.PI / 2 - tVertexAngle / 2;
+    var tAngleEnd = Math.PI / 2 + tVertexAngle / 2;
+    var xc = cWidth / 2; var yc = cHeight / 2;
+    var x0 = xc - tWidth / 2; var y0 = yc + tHeight / 2;
+    var x1 = xc; var y1 = yc - tHeight / 2;
+    var x2 = xc + tWidth / 2; var y2 = yc + tHeight / 2;
+
+    ctx.beginPath();
+    ctx.strokeStyle = coinStroke;
+    ctx.fillStyle = coinFill;
+    ctx.lineWidth = 2;
+    ctx.moveTo(x0, y0);
+    ctx.lineTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x0, y0);
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(x1, y1, 20, tAngleStart, tAngleEnd);
+    ctx.stroke();
+
+    ctx.fillStyle = 'black';
+
+    var xTheta = x1 - 2;
+    var yTheta = y1 + 30;
+    ctx.fillText('θ', xTheta, yTheta);
+
+    var xZ = x1 - 2;
+    var yZ = y1 + tHeight + 10;
+    ctx.fillText('z', xZ, yZ);
+
+    var xX = x1 - 10 - 0.25 * tWidth;
+    var yX = y1 + 0.5 * tHeight;
+    ctx.fillText('x', xX, yX);
+
+    var xY = x1 + 6 + 0.25 * tWidth;
+    var yY = y1 + 0.5 * tHeight;
+    ctx.fillText('y', xY, yY);
+
 }
 
 function drawRing(ctx, a0, n) {
@@ -105,8 +216,6 @@ function drawThreeCoins(ctx) {
         var t = j * 2 * Math.PI / 7;
         drawOffsetCircle(ctx, yc, xc, rDemo2, rDemo, t)
     }
-    // drawOffsetCircle(ctx, xc, yc, rDemo2, rDemo, 0);
-    // drawOffsetCircle(ctx, xc, yc, rDemo2, rDemo, tDemo);
 
     var ct = Math.cos(tDemo);
     var st = Math.sin(tDemo);
