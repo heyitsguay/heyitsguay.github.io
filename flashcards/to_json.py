5f8485cbcb68439f6bd83c71973c5947f986adad
Matthew Guay <matthew.guay@nih.gov>
2019-07-22 19:44:18 -0400

    Swedish flashcard stuff

--- flashcards/to_json.py
+++ flashcards/to_json.py
@@ -4,7 +4,7 @@ import json
 def main():
 
     translation_pairs = parse_manual_list('wordslearned.md')
-    with open('worldslearned.json', 'w') as f:
+    with open('wordslearned.json', 'w') as f:
         json.dump(translation_pairs, f)
 
     pass
@@ -15,12 +15,25 @@ def parse_manual_list(l):
     with open(l, 'r') as f:
         text = f.read().lower()
     lines = text.split('\n')
-    word_lines = [l for l in lines if l[0] == '_']
-    for line in world_lines:
+    word_lines = [l for l in lines if len(l) > 0 and l[0] == '_']
+    for line in word_lines:
         entries = line.split('; from ')
         for entry in entries:
             e_swedish, e_english = entry.split(': ')
             word_swedish = e_swedish.replace('_', '')
             translations_english = e_english.split(', ')
-            translation_pairs.append(word_swedish, translations_english)
+            for i, translation in enumerate(translations_english):
+                if '(' in translation and ')' in translation:
+                    lparen_pos = translation.find('(')
+                    rparen_pos = translation.find(')')
+                    paren_contents = translation[lparen_pos+1:rparen_pos]
+                    translation1 = translation[:lparen_pos]
+                    translation2 = translation1 + paren_contents
+                    translations_english[i] = translation1
+                    translations_english.append(translation2)
+            translation_pairs.append((word_swedish, translations_english))
     return translation_pairs
+
+
+if __name__ == '__main__':
+    main()
--- flashcards/wordslearned.md
+++ flashcards/wordslearned.md
@@ -1,5 +1,29 @@
 # Words (and phrases) I have learned
 
+#### July 7
+_förbättra_: improve
+_förberett_: prepared; from _förbereda_: prepare
+_saknas_: be lacking
+_vårdpersonal_: health professional(s)
+_ta hand om_: take care of
+_myndighet_: authority
+_samhälle_: society
+_riksdagsparti_: parliament party
+_nyhet_: novelty, new thing
+_framtid_: future
+_stöd_: support
+_vård_: care
+_sämre_: worse
+_minska_: reduce
+_fackförening_: union
+_an_: to
+_bjuda_: invite
+_landsting_: county council(s)
+_utveckla_: develop
+_vara klar_: be ready
+_klar_: clear
+
+
 #### July 5
 _häktad_: arrested; from _häkta_: arrest, detain
 _tingsrätt_: district court
